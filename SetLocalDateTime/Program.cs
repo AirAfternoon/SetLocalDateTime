4f3f6b47300412caf1c38aefeee45f51ff7199c3
huangyinkun <huangyinkun@easyretailpro.com>
2019-04-22 17:59:32 +0800

    create

---
+++ SetLocalDateTime/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+namespace SetLocalDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            SetTimeZone(new TimeSpan(-14, 0, 0));
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now);
+            SetTimeZone(new TimeSpan(14, 0, 0));
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now);
+        }
+
+        static void SetTimeZone(TimeSpan ts)
+        {
+            var now = DateTime.Now;
+            var settingTS = ts;
+            TimeZoneInfo settingTZI = TimeZoneInfo.CreateCustomTimeZone("1", settingTS, "测试时区", "测试时区");
+
+            object cachedData = typeof(TimeZoneInfo).GetField("s_cachedData", BindingFlags.NonPublic | BindingFlags.Static).GetValue(settingTZI);
+
+            var typeCachedData = cachedData.GetType();
+            typeCachedData.GetField("_localTimeZone", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(cachedData, settingTZI);
+            object oneYearLocalFromUtc = typeCachedData.GetField("_oneYearLocalFromUtc", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(cachedData);
+
+            var fieldTS = oneYearLocalFromUtc.GetType().GetField("Offset", BindingFlags.Public | BindingFlags.Instance);
+            fieldTS.SetValue(oneYearLocalFromUtc, settingTS);
+        }
+
+    }
+}
